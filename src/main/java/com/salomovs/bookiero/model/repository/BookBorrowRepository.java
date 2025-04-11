2662ed1c3016b07e8134b55506787192eb471a69
Salomao S. <souza95salomao@gmail.com>
2025-04-11 17:26:19 +0000

    :sparkles: feat: asna user, one can now borrow existing books and prevent duplicated borrowing

--- src/main/java/com/salomovs/bookiero/model/repository/BookBorrowRepository.java
+++ src/main/java/com/salomovs/bookiero/model/repository/BookBorrowRepository.java
@@ -2,12 +2,20 @@ package com.salomovs.bookiero.model.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.salomovs.bookiero.model.entity.BookBorrow;
 
 @Repository
 public interface BookBorrowRepository extends CrudRepository<BookBorrow, Integer> {
-  List<BookBorrow> findAllByBookId(Integer bookId);
+  @Query(
+    nativeQuery=true, 
+    value="SELECT * FROM borrows b " +
+          "WHERE b.book_id = :book_id " +
+          "AND b.returned_at IS NULL"
+  )
+  List<BookBorrow> findByBookId(@Param("book_id") Integer bookId);
 }
