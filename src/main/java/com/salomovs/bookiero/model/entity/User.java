a1abc3fe000e3ca246beea8f89282c9ec8c58a54
Salomao S. <souza95salomao@gmail.com>
2025-04-15 21:30:41 +0000

    :recycle: fix: removes sensitive user info from view response in json

--- src/main/java/com/salomovs/bookiero/model/entity/User.java
+++ src/main/java/com/salomovs/bookiero/model/entity/User.java
@@ -1,5 +1,7 @@
 package com.salomovs.bookiero.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,7 +26,7 @@ public class User {
   @Column(name="tax_id", nullable=false)
   private String taxId;
 
-  @Column(nullable=false)
+  @Column(nullable=false) @JsonIgnore
   private String password;
 
   @Column(nullable=false)
