e584ee8639bbc082fef8798b0b375284878f5d20
Salomao S. <souza95salomao@gmail.com>
2025-04-23 18:03:09 +0000

    :sparkles: feat: as a user, one can now login using existing account and receive userdata and a token for subsequent requests

--- src/main/java/com/salomovs/bookiero/view/AuthView.java
+++ src/main/java/com/salomovs/bookiero/view/AuthView.java
@@ -1,13 +1,20 @@
 package com.salomovs.bookiero.view;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.salomovs.bookiero.controller.AuthController;
+import com.salomovs.bookiero.model.entity.User;
+import com.salomovs.bookiero.utils.JwtUtil;
 import com.salomovs.bookiero.view.dto.HttpResponse;
+import com.salomovs.bookiero.view.dto.LoginResponse;
+import com.salomovs.bookiero.view.dto.UserLoginDto;
 import com.salomovs.bookiero.view.dto.UserSignUpDto;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,9 +28,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @RestController @RequestMapping("/api/auth")
 public class AuthView {
   private AuthController authController;
+  private AuthenticationManager authManager;
+  private JwtUtil jwtUtil;
 
-  public AuthView(final AuthController authController) {
+  public AuthView(final AuthController authController,
+                  final AuthenticationManager authManager,
+                  final JwtUtil jwtUtil) {
     this.authController = authController;
+    this.authManager = authManager;
+    this.jwtUtil = jwtUtil;
   }
 
   @Operation(summary="User Registration Handler") @PostMapping("/signup")
@@ -39,7 +52,20 @@ public class AuthView {
     )
   })
   public ResponseEntity<HttpResponse> handleSignUp(@RequestBody UserSignUpDto body) {
-    int newUserId = this.authController.register(body);
+    int newUserId = this.authController.saveUserInfo(body);
     return ResponseEntity.status(201).body(new HttpResponse(true, newUserId));
+  }
+
+  @Operation(summary="User Authentication Handler") @PostMapping("/login")
+  public ResponseEntity<LoginResponse> handleLogin(@RequestBody UserLoginDto body) {
+    Authentication authentication = this.authManager.authenticate(
+      UsernamePasswordAuthenticationToken.unauthenticated(body.credential(), body.password())
+    );
+
+    User userDetails = (User) authentication.getPrincipal();
+    String jwtToken = this.jwtUtil.issueToken(userDetails);
+    LoginResponse res = new LoginResponse(userDetails, jwtToken);
+
+    return ResponseEntity.status(200).body(res);
   }
 }
