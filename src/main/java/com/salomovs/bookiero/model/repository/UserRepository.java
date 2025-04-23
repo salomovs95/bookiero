f7c97530376cd8974d6ff205d82a97f0abf07682
Salomao S. <souza95salomao@gmail.com>
2025-04-23 17:58:08 +0000

    :wrench: update ueer repository to find an user by either username or email in same query

--- src/main/java/com/salomovs/bookiero/model/repository/UserRepository.java
+++ src/main/java/com/salomovs/bookiero/model/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package com.salomovs.bookiero.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.salomovs.bookiero.model.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+  @Query(
+    value="SELECT * FROM users u WHERE u.username = :credential OR u.email = :credential",
+    nativeQuery=true
+  )
+  public Optional<User> findByUsernameOrEmail(@Param("credential") String credential);
+}
