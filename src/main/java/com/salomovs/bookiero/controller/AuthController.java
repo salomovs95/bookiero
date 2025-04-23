9c28623837dde1e747e23eaecf82bb88be28fa18
Salomao S. <souza95salomao@gmail.com>
2025-04-23 17:54:18 +0000

    :wrench: update: search user function and adds password encryption upon registration

--- src/main/java/com/salomovs/bookiero/controller/AuthController.java
+++ src/main/java/com/salomovs/bookiero/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.salomovs.bookiero.controller;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.salomovs.bookiero.exception.UserNotFoundException;
@@ -8,30 +10,47 @@ import com.salomovs.bookiero.model.repository.UserRepository;
 import com.salomovs.bookiero.view.dto.UserSignUpDto;
 
 @Service
-public class AuthController {
+public class AuthController implements UserDetailsService {
   private UserRepository userRepo;
+  private PasswordEncoder pwEncoder;
 
-  public AuthController(final UserRepository userRepo) {
+  public AuthController(final UserRepository userRepo,
+                        final PasswordEncoder passwordEncoder) {
     this.userRepo = userRepo;
+    this.pwEncoder = passwordEncoder;
   }
 
-  public Integer register(UserSignUpDto dto) {
+  public Integer saveUserInfo(UserSignUpDto dto) {
+    String encodedPassword = this.pwEncoder.encode(dto.password());
+    
     User newUser = this.userRepo.save(new User(
       null,
       dto.fullName(),
+      dto.username(),
       dto.taxId(),
-      dto.password(),
+      encodedPassword,
       dto.email(),
       dto.phone(),
-      dto.address()
+      dto.address(),
+      "USERS.COMMON"
     ));
 
     return newUser.getId();
   }
 
   public User getUserInfo(Integer userId) {
-    User user = userRepo.findById(userId)
+    User user = this.userRepo
+      .findById(userId)
       .orElseThrow(()->new UserNotFoundException(userId));
+
+    return user;
+  }
+
+  public User loadUserByUsername(String credential) {
+    User user = this.userRepo
+      .findByUsernameOrEmail(credential)
+      .orElseThrow(()->new UserNotFoundException(credential));
+
     return user;
   }
 }
