1d2a40937db962da95281ee0f76dc9e9691e5042
Salomao S. <souza95salomao@gmail.com>
2025-04-25 13:35:58 +0000

    :sparkles: feat: as an user, one can now login and get issued a jwt token for subsequent requests

--- src/main/java/com/salomovs/bookiero/view/AuthView.java
+++ src/main/java/com/salomovs/bookiero/view/AuthView.java
@@ -1,9 +1,22 @@
 package com.salomovs.bookiero.view;
 
+import java.time.Instant;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,35 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.salomovs.bookiero.controller.AuthController;
 import com.salomovs.bookiero.model.entity.User;
-import com.salomovs.bookiero.utils.JwtUtil;
 import com.salomovs.bookiero.view.dto.HttpResponse;
 import com.salomovs.bookiero.view.dto.LoginResponse;
 import com.salomovs.bookiero.view.dto.UserLoginDto;
 import com.salomovs.bookiero.view.dto.UserSignUpDto;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 @Tag(name="Authentication View", description="Handles user's registration and authentication")
 @RestController @RequestMapping("/api/auth")
 public class AuthView {
   private AuthController authController;
   private AuthenticationManager authManager;
-  private JwtUtil jwtUtil;
+  private JwtEncoder jwtEncoder;
 
   public AuthView(final AuthController authController,
                   final AuthenticationManager authManager,
-                  final JwtUtil jwtUtil) {
+                  final JwtEncoder encoder
+  ) {
     this.authController = authController;
     this.authManager = authManager;
-    this.jwtUtil = jwtUtil;
+    this.jwtEncoder = encoder;
   }
 
-  @Operation(summary="User Registration Handler") @PostMapping("/signup")
+  @Operation(
+    summary="User Registration Handler",
+    security={@SecurityRequirement(name="")}
+  )
   @ApiResponses({
     @ApiResponse(
       responseCode="201",
@@ -51,21 +60,37 @@ public class AuthView {
       }
     )
   })
+  @PostMapping("/signup")
   public ResponseEntity<HttpResponse> handleSignUp(@RequestBody UserSignUpDto body) {
     int newUserId = this.authController.saveUserInfo(body);
     return ResponseEntity.status(201).body(new HttpResponse(true, newUserId));
   }
 
-  @Operation(summary="User Authentication Handler") @PostMapping("/login")
+  @Operation(
+    summary="User Authentication Handler",
+    security={@SecurityRequirement(name="")}
+  )
+  @PostMapping("/login")
   public ResponseEntity<LoginResponse> handleLogin(@RequestBody UserLoginDto body) {
     Authentication authentication = this.authManager.authenticate(
       UsernamePasswordAuthenticationToken.unauthenticated(body.credential(), body.password())
     );
 
     User userDetails = (User) authentication.getPrincipal();
-    String jwtToken = this.jwtUtil.issueToken(userDetails);
-    LoginResponse res = new LoginResponse(userDetails, jwtToken);
+    String jwtToken = this.jwtEncoder.encode(
+      JwtEncoderParameters.from(
+        JwtClaimsSet
+          .builder()
+          .claim("username", userDetails.getUsername())
+          .claim("role", userDetails.getRole())
+          .issuedAt(Instant.now())
+          .expiresAt(Instant.now().plusSeconds(60*60))
+          .subject(userDetails.getEmail())
+          .build()
+      )
+    ).getTokenValue();
 
-    return ResponseEntity.status(200).body(res);
+    LoginResponse response = new LoginResponse(userDetails, jwtToken);
+    return ResponseEntity.status(200).body(response);
   }
 }
