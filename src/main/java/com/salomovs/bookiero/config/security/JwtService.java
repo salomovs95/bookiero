3b0b3fcc202b1aea2deeaf5ebcd52815381131c5
Salomao S. <souza95salomao@gmail.com>
2025-05-10 21:54:44 +0000

    :recycle: refactor: enchances authentication/authorization related configs

---
+++ src/main/java/com/salomovs/bookiero/config/security/JwtService.java
@@ -0,0 +1,48 @@
+package com.salomovs.bookiero.config.security;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+@Component
+public class JwtService {
+  @Value("${spring.security.jwt.secret}")
+  private String jwtSecret;
+  @Value("${spring.security.jwt.issuer}")
+  private String jwtIssuer;
+
+  private Long tokenExpirationInSeconds = 60 * 60l;
+
+  public String generateToken(UserDetails user) {
+    var claims = user.getAuthorities()
+                     .stream()
+                     .map(authority->authority.getAuthority())
+                     .collect(Collectors.toList());
+
+    return JWT.create()
+              .withIssuer(jwtIssuer)
+              .withExpiresAt(Instant.now().plusSeconds(tokenExpirationInSeconds))
+              .withSubject(user.getUsername())
+              .withClaim("auhtorities", claims)
+              .sign(getAlgorithm());
+  }
+
+  public String verify(String token) {
+    return JWT.require(getAlgorithm())
+              .withIssuer(jwtIssuer)
+              .build()
+              .verify(token)
+              .getSubject();
+  }
+
+  private Algorithm getAlgorithm() {
+    return Algorithm.HMAC256(jwtSecret.getBytes());
+  }
+}
---
+++ src/main/java/com/salomovs/bookiero/config/security/PasswordEncoding.java
@@ -0,0 +1,21 @@
+package com.salomovs.bookiero.config.security;
+
+import java.security.SecureRandom;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoding {
+  @Value("${spring.security.secret-random}")
+  private String randomSecret;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    SecureRandom secureRandom = new SecureRandom(randomSecret.getBytes());
+    return new BCryptPasswordEncoder(16, secureRandom);
+  }
+}
