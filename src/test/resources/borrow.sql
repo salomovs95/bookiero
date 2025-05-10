bc50f5df88df0e5646d1d00d59e5766b5dd227e4
Salomao S. <souza95salomao@gmail.com>
2025-05-10 21:52:14 +0000

    :seedling: chore: update tests sql seed file

--- src/test/resources/borrow.sql
+++ src/test/resources/borrow.sql
@@ -1,7 +1,9 @@
+INSERT INTO authors (id, full_name, profile_picture) VALUES (95, 'Salo Movs', 'https://github.com/salomovs95.png');
+
 INSERT INTO users (id, full_name, username, tax_id, phone, email, password, address, role) VALUES (61, 'user-001', 'user-001', 'user-001-tax-id', 'user-001-phone', 'user-001-email', 'user-001@password', 'user-001 st., ivalice', 'USERS.COMMON');
 
 INSERT INTO users (id, full_name, username, tax_id, phone, email, password, address, role) VALUES (62, 'user-002', 'user-002', 'user-002-tax-id', 'user-002-phone', 'user-002-email', 'user-002@password', 'user-002 st., ivalice', 'USERS.COMMON');
 
 INSERT INTO users (id, full_name, username, tax_id, phone, email, password, address, role) VALUES (63, 'user-003', 'user-003', 'user-003-tax-id', 'user-003-phone', 'user-003-email', 'user-003@password', 'user-003 st., ivalice', 'USERS.COMMON');
 
-INSERT INTO books (id, title, author_name, page_count, esbn, edition, editor, publish_year, category, in_stock_amount) VALUES (11, 'book-001', 'salomovs', 468, 'book-001-esbn-code', '1st ed.', 'salomovs inc.', 2025, 'dev, sci-fi', 2);
+INSERT INTO books (id, title, author_id, page_count, esbn, edition, editor, publish_year, category, in_stock_amount) VALUES (11, 'book-001', 95, 468, 'book-001-esbn-code', '1st ed.', 'salomovs inc.', 2025, 'dev, sci-fi', 2);
