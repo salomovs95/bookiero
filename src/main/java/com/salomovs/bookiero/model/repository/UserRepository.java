a63dcea64785c4159e7d32024ce7d89b9548169c
Salomao S. <souza95salomao@gmail.com>
2025-05-27 16:54:58 +0000

    :card_file_box: updates native queries in repositories

--- src/main/java/com/salomovs/bookiero/model/repository/UserRepository.java
+++ src/main/java/com/salomovs/bookiero/model/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.salomovs.bookiero.model.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,14 +11,11 @@ import com.salomovs.bookiero.model.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-  @Query(
-    value="""
-      SELECT * 
-      FROM users u 
-      WHERE u.username = :credential 
-      OR u.email = :credential
-    """,
-    nativeQuery=true
-  )
+  @NativeQuery("""
+    SELECT * 
+    FROM users u 
+    WHERE u.username = :credential 
+    OR u.email = :credential
+  """)
   public Optional<User> findByUsernameOrEmail(@Param("credential") String credential);
 }
